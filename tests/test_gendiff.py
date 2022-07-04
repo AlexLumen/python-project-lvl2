95486b4f29f8baf653d4dcfcee680270f87452c9
Aleksey_Vedin <aleksey.vedin@rt.ru>
2022-07-04 16:47:15 +0400

    fix

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+"""
 from gendiff.generate_diff import generate_diff
 import json
 import yaml
@@ -14,7 +14,7 @@ def test_generate_diff_with_identical_files_json():
     assert diff == result
 
 
-"""
+
 def test_diff_with_first_empty_file_json():
     result = open('tests/fixtures/test2_result.txt', 'r').read()
     file_path_1 = json.load(open('tests/fixtures/test2_file1.json'))
@@ -31,7 +31,7 @@ def test_diff_with_second_empty_file_json():
     file_path_2 = json.load(open('tests/fixtures/test3_file2.json'))
     diff = generate_diff(file_path_1, file_path_2)
     assert diff == result
-"""
+
 
 
 def test_diff_files_json():
@@ -52,7 +52,7 @@ def test_generate_diff_with_identical_files_yaml():
     assert diff == result
 
 
-"""
+
 def test_diff_with_first_empty_file_yaml():
     result = open('tests/fixtures/test2_result.txt', 'r').read()
     file_path_1 = yaml.load(open('tests/fixtures/test2_file1.yaml'))
@@ -68,7 +68,7 @@ def test_diff_with_second_empty_file_yaml():
     diff = generate_diff(file_path_1, file_path_2)
     assert diff == result
 
-"""
+
 
 
 def test_diff_files_yaml():
@@ -77,3 +77,4 @@ def test_diff_files_yaml():
     file_path_2 = yaml.load(open('tests/fixtures/test4_file2.yaml'))
     diff = generate_diff(file_path_1, file_path_2)
     assert diff == result
+"""
